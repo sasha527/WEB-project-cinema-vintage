fd011e81728a02b5d6316f01750d93dfdba550ee
Форостян Олександр bc2024-5 <forostiansasha13@gmail.com>
2025-05-25 18:21:57 +0300

    Підключено ToastContainer з react-toastify для глобального відображення повідомлень

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,24 @@
-﻿import './index.css';   
+﻿// src/main.jsx
+import './index.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BookingProvider } from './pages/BookingContext';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BookingProvider>
+    <>
+      <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="colored"
+      />
+    </>
+  </BookingProvider>
+);
